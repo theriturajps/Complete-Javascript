2c8940f914ba82874751b2b0537a86ad60aeed81
theriturajps <theriturajps@gmail.com>
2024-09-28 11:08:12 +0530

    updated

---
+++ 9th.js
@@ -0,0 +1,48 @@
+////////////////////////////// Classed & Objects //////////////////////////////
+
+const stud = {
+    name: "raj",
+    class: "bsc IT",
+    roll: 9,
+    phn: 9000000001,
+    marks: function (){
+        // console.log(`your phn is `, this.phn);
+    }
+}
+
+
+const gst = {
+    tax1: function(){
+        console.log("the tax is 2%");
+    },
+    tax2(){
+        console.log("the tax is 12%");
+    }
+}
+
+////////// Prototype -> __proto__
+
+const empl = {
+    salary: 20000
+}
+empl.__proto__ = gst;
+
+const empl1 = {
+    salary: 20000
+}
+empl1.__proto__ = gst;
+
+const empl2 = {
+    salary: 20000
+}
+empl2.__proto__ = gst;
+
+const empl3 = {
+    salary: 20000
+}
+empl3.__proto__ = gst;
+
+
+//////////////////// Classes
+
+
