3006c6cb5f760af8622c8795b413ac62c587ec93
theriturajps <theriturajps@gmail.com>
2024-09-28 15:53:07 +0530

    updated

---
+++ 10th.js
@@ -0,0 +1,24 @@
+////////////////// async await >> promise chains >> callback hell //////////////////
+
+
+/////// Synchoronous Programming
+
+// console.log("one");
+// console.log("two");
+// console.log("three");
+
+function hello (){
+    // console.log("hello");
+}
+setTimeout(hello, 2000) // 2s = 2000ms
+
+
+///// ************************************************** ///////
+
+setTimeout(() =>{
+    // console.log("hello");
+}, 2000) 
+
+
+/////// Callback 
+
