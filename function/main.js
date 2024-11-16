4fbbabf615ea340180cb0eb1318bbd0ae0bed2ad
theriturajps <theriturajps@gmail.com>
2024-11-16 15:54:27 +0530

    1554

--- function/main.js
+++ function/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Default parameters
+/////////// Default parameters
 
 const bookings = []
 
@@ -78,7 +78,7 @@ checKIn(flight, raj)
 
 console.log('--------------------------------------------------------')
 
-// Functions Accepting Callback Functions
+/////////// Functions Accepting Callback Functions
 
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase()
@@ -90,7 +90,7 @@ const upperFirstWord = function (str) {
 }
 
 
-// higher order function
+/////////// higher order function
 const transformer = function (str, fn) {
 	console.log(`Original string : ${str}`);
 	console.log(`Transformed string : ${fn(str)} by - ${fn.name}`);	
@@ -98,3 +98,81 @@ const transformer = function (str, fn) {
 
 transformer('Javascript is the best', oneWord) // callback function
 transformer('Javascript is the best', upperFirstWord) // callback function
+
+
+
+/////////// Function returning Function
+
+const greet = (greeting) => {
+	return (name) => {
+		console.log(`${greeting} - ${name}`);
+	}
+}
+
+const greetFnc = greet('Hi')
+
+greetFnc('Raj') // Hi - Raj
+greet('Hi')('Raj') // Hi - Raj
+
+
+
+/////////// Arrow Way
+
+const greetArr = (greeting) => (name) => console.log(`${greeting} - ${name}`)
+greetArr('Hi')('Raj') // Hi - Raj
+
+
+/////////// The call and apply methods
+
+
+const airIndia = {
+	airline: 'Air india',
+	iataCode: 'AI',
+	booking: [],
+
+	book: function (flightNum, name) {
+		console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode} - ${flightNum}`);
+		this.booking.push({
+      flight: `${this.iataCode}${flightNum}`,
+			passName: name,
+		})
+	}
+
+}
+
+
+airIndia.book('243', 'Ritu')
+airIndia.book('567', 'Ram')
+airIndia.book('123', 'Neha')
+
+//////////////////////////////////////////////////////////////////////////////
+
+// const book = airIndia.book
+
+
+// const euroWIngs = {
+// 	name: 'EuriWings',
+// 	iataCode: 'EW',
+// 	booking: [],
+// 	book
+// }
+
+
+// euroWIngs.book(23, 'Khushi')
+
+//////////////////////////////////////////////////////////////////////////////
+
+const euroWIngs = {
+	name: 'EuriWings',
+	iataCode: 'EW',
+	booking: []
+}
+
+// Does not work
+// euroWIngs.book(23, 'Khushi')
+
+const book = airIndia.book
+
+book.call(euroWIngs, 234, 'Neha') // call apply bind
+
+book.call(airIndia, 897, 'Komal')
